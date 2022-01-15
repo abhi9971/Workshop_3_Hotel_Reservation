c1ca321b358cc7accea1e94f1adf0d6acacd9bcb
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-15 14:18:15 +0530

    Added : Ability to find cheapest hotel for given range of date.

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,7 +1,18 @@
-package com.bridgelabz.workshop3.advancejava;
+package com.brigelabz.workshop3;
 
 public class HotelReservationSystem {
     public static void main(String[] args) {
-        System.out.println(" Welcome to Hotel Reservation Program ");
+        System.out.println("Welcome to Hotel Reservation System");
+        HotelSetup hotelSetup=new HotelSetup();
+        hotelSetup.addHotel();
+        hotelSetup.showHotelInfo();
+
+
+        System.out.println(" ");
+        System.out.println("*Cheapest Hotel Based on WeekdayRegularRate");
+        hotelSetup.findCheapestHotelOne("01-Jan-2021","05-Jan-2021");
+        System.out.println(" ");
+
+
     }
 }
