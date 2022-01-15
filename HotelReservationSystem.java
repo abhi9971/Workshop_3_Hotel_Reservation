760c3a325abc1643b39cced3df6c2a3b550eb505
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-15 09:11:00 +0530

    Add Hotels names with Rating

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,7 +1,9 @@
-package com.bridgelabz.workshop3.advancejava;
+package com.brigelabz.workshop3;
 
 public class HotelReservationSystem {
     public static void main(String[] args) {
-        System.out.println(" Welcome to Hotel Reservation Program ");
+        System.out.println("Welcome to Hotel Reservation System");
+        HotelSetup hotelSetup=new HotelSetup();
+        hotelSetup.addHotel();
     }
 }
